b5c1065a723c6ab5c2be0f159ccebbed8a08f0c7
Adler Santos <adlercoelhosantos12@gmail.com>
2024-05-21 19:25:42 -0300

    Criando tela de Login do MentorMatch.

---
+++ app/src/main/java/br/com/fiap/mentormatch/Login.kt
@@ -0,0 +1,50 @@
+package br.com.fiap.mentormatch
+
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import br.com.fiap.mentormatch.databinding.ActivityMainBinding
+
+class Login: AppCompatActivity(R.layout.login)  {
+
+    private lateinit var binding: ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+
+        val btnLogar: ImageButton = findViewById(R.id.btn_login);
+        btnLogar.setOnClickListener {
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            enableEdgeToEdge()
+            setContentView(binding.root)
+            replaceFragment(Home())
+
+            binding.bottomNavigationView.setOnItemSelectedListener {
+                when (it.itemId) {
+                    R.id.home -> replaceFragment(Home())
+                    R.id.chat -> replaceFragment(Chat())
+                    R.id.perfil -> replaceFragment(Perfil())
+                }
+                true
+            }
+
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+
+}
--- app/src/main/java/br/com/fiap/mentormatch/Welcome.kt
+++ app/src/main/java/br/com/fiap/mentormatch/Welcome.kt
@@ -1,15 +1,9 @@
 package br.com.fiap.mentormatch
 
-import android.app.FragmentTransaction
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import br.com.fiap.mentormatch.databinding.ActivityMainBinding
 
 class Welcome : AppCompatActivity(R.layout.welcome) {
@@ -17,37 +11,13 @@ class Welcome : AppCompatActivity(R.layout.welcome) {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState)
 
-        val btnLogar: ImageButton = findViewById(R.id.btn_login);
+        val btnLogar: ImageButton = findViewById(R.id.btn_login)
         btnLogar.setOnClickListener {
-            binding = ActivityMainBinding.inflate(layoutInflater)
-            enableEdgeToEdge()
-            setContentView(binding.root)
-            replaceFragment(Home())
-
-            binding.bottomNavigationView.setOnItemSelectedListener {
-                when(it.itemId){
-                    R.id.home -> replaceFragment(Home())
-                    R.id.chat -> replaceFragment(Chat())
-                    R.id.perfil -> replaceFragment(Perfil())
-                }
-                true
-            }
-
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-                insets
-            }
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
         }
-    }
-
-    private fun replaceFragment(fragment: Fragment){
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frameLayout, fragment)
-        fragmentTransaction.commit()
     }
 
 }
